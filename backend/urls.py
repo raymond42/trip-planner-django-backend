437eb888bd7a1b8136ec1fbcc5205eead3ae04cb
Raymond <raymond42.gr@gmail.com>
2025-03-25 17:32:00 +0200

    versioning the apis

--- backend/urls.py
+++ backend/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-        path('api/', include('trips.urls')),
+        path('api/v1/', include('trips.urls')),
 ]
